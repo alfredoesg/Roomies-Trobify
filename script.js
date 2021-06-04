55e25c92d99ef17e9eb0416574b37d95174877ff
Enrique1804 <kikr.ragu18@gmail.com>
2021-06-03 22:19:36 -0500

    apis

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-// Initialize and add the map
-function initMap() {
-	// The location of Uluru
-	const uluru = { lat: -25.344, lng: 131.036 };
-	// The map, centered at Uluru
-	const map = new google.maps.Map(document.getElementById("gugelmaps"), {
-	  zoom: 4,
-	  center: uluru,
-	});
-	// The marker, positioned at Uluru
-	const marker = new google.maps.Marker({
-	  position: uluru,
-	  map: map,
-	});
-  }
+function initMap(){
+	var coord = {lat:19.42847 ,lng:-99.12766};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 13,
+      center: coord
+    });
+}
